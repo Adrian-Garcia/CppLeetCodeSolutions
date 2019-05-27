6f03e042646de9d6a1d056f49268dce4901bb817
Adrian Garcia <adriangarlop@hotmail.com>
2019-05-27 11:09:01 -0500

    Changed myAtoid function

--- atoi.cpp
+++ atoi.cpp
@@ -5,14 +5,51 @@ using namespace std;
 
 int myAtoi(string str) {
 
-	int res;
+	int res, size;
+	int counter = 0;
+	int i=0;
+	
+	bool flag = false;
+	bool positive = true;
 
-	return res;
+	while (str[i] == ' ') {
+		flag = true;
+		counter++;
+		i++;
+	}
+
+	if (flag) str.erase(0, counter);
+
+	if (isalpha(str[0]) && str[0] != '-')
+		return 0;
+
+	i = (str[0] == '-') ? 1 : 0;
+	flag = false;
+	counter = 0;
+
+	while(isdigit(str[i])) {
+		counter++;
+		flag = true;
+		i++;
+	}
+
+	if (flag) str.erase(counter, str.length()-1);
+
+	positive = (str[0] != '-') ? true : false;
+
+	size = (positive) ?
+		str.length()-1 : str.length();
+
+	// return res;
+	return 1;
 }
 
 int main() {
 
-	
+	string str = "   42 asddd";
+
+	int number = myAtoi(str);
+	// cout << number-1 << endl;
 
 	return 0;
 }
