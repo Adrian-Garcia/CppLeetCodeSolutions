a82ee69727ce91a5953fa713674fa1c7ac53e672
Adrian Garcia <adriangarlop@hotmail.com>
2019-07-27 12:52:50 -0500

    finished first prototype of BinaryTree.cpp it is not compiled

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -8,6 +9,123 @@ struct TreeNode {
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
+void add(TreeNode *root, int data) {
+
+	TreeNode *curr = root;
+	TreeNode *father = NULL;
+
+	while (curr != NULL) {
+
+		if (curr->val == data) 
+			return;
+
+		father = curr;
+
+		curr = (data < curr->val) ?
+			curr->left : curr->right; 
+	}
+
+	if (father->val > data) {
+		father->left = new TreeNode(data);
+	} else {
+		father->right = new TreeNode(data);
+	}
+}
+
+bool search(TreeNode *root, int data) {
+
+	TreeNode *curr = root;
+
+	while (curr != NULL) {
+
+		if (curr-> val == data)
+			return true;
+
+		curr = (curr->val > data) ?
+			curr->left : curr->right;
+	}
+
+	return false;
+
+}
+
+void preOrder(TreeNode *r) {
+	
+	if (r != NULL) {
+		cout << r->val << " ";
+		preOrder(r->left);
+		preOrder(r->right);
+	}
+}
+
+void inOrder(TreeNode *r) {
+
+	if (r != NULL) {
+		inOrder(r->left)
+		cout << r->val << " ";
+		inOrder(r->right);
+	}
+}
+
+void postOrder(TreeNode *r) {
+
+	if (r != NULL) {
+		postOrder(r->left);
+		postOrder(r->right);
+		cout << r->val << " ";
+	}
+}
+
+void levels(TreeNode *root) {
+
+	queue<TreeNode*> nodes;
+
+	nodes.push(root);
+
+	while (!nodes.empty()) {
+
+		if (nodes.front()->left != NULL)
+			nodes.push(nodes.front()->left);
+
+		if (nodes.front()->right != NULL)
+			nodes.push(nodes.front()->right);
+
+		cout << nodes.front()->val << " ";
+
+		nodes.pop();
+	}
+}
+
+int whatLevelAmI(TreeNode *root, int data) {
+
+	TreeNode *curr = root;
+	int level = 0;
+
+	while (curr != NULL) {
+
+		if (curr->val == data)
+			return level;
+
+		curr = (curr->val > data) ?
+			curr->left : curr->right;
+
+		level++;
+	}
+
+	return -1;
+}
+
+int height(TreeNode *r) {
+
+	if (r == NULL)
+		return 0;
+
+	int left = height(r->left);
+	int right = height(r->right);
+
+	return 1 + (izq > der ? izq : der);
+}
 
 int main() {
 
