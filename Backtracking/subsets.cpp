81b52760e91cf7522188436ec120c5173236fd52
Adrian Garcia <adriangarlop@hotmail.com>
2019-08-20 09:35:06 -0500

    added subsests

--- Backtracking/subsets.cpp
+++ Backtracking/subsets.cpp
@@ -71,6 +71,9 @@ int main() {
 	nums.push_back(1);
 	nums.push_back(2);
 	nums.push_back(3);
+	// nums.push_back(4);
+	// nums.push_back(5);
+	// nums.push_back(6);
 
 	vector<vector<int> > response = subsets(nums);
 
