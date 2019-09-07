bad965a93a0677246d0b096f7c4e4651f89c8f3c
Adrian Garcia <adriangarlop@hotmail.com>
2019-09-07 17:13:15 -0500

    Added biggestSum.cpp

--- Backtracking/Knapsack.cpp
+++ Backtracking/Knapsack.cpp
@@ -7,8 +7,6 @@ using namespace std;
 // Get maximum value of two numbers
 int max(int a, int b) { 
 
-	cout << a << " " << b << endl;
-
 	return (a > b) ?
 		a : b; 
 }
@@ -19,22 +17,25 @@ int knapSack(int W, vector<int> weightThing, vector<int> values, int i) {
 	if (i == 0 || W == 0)  
 		return 0;  
 
-	if (weightThing[i-1] > W)  
-		return knapSack(W, weightThing, values, i-1);  
+	if (weightThing[i-1] > W)
+		return knapSack(W, weightThing, values, i-1);
 	  
 	else return max(values[i-1]+knapSack(W-weightThing[i-1], weightThing, values, i-1), knapSack(W, weightThing, values, i-1) );  
 }  
   
 int main() {
 
-	vector<int> values;
-	vector<int> weightThing;
-	int W = 12; 		// Weight
+	vector<int> values;			// Value of things
+	vector<int> weightThing;	// Weight of indivudual things
+	int W = 12; 				// Weight
+
 	
+	values.push_back(20);
 	values.push_back(1);
 	values.push_back(2);
 	values.push_back(3);
 
+	weightThing.push_back(300000);
 	weightThing.push_back(5);
 	weightThing.push_back(6);
 	weightThing.push_back(7);
