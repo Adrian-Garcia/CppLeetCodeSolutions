5b189d12c5723e542cb6047c7343a97897477fd2
Adrian Garcia <adriangarlop@hotmail.com>
2019-09-08 16:01:40 -0500

    Added anagrams.cpp

---
+++ Map/anagrams.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void getPlaces(map<char, vector<int> > &places, string word) {
+
+	for (int i=0; i<word.length(); i++) {
+		if (!places.count(word[i]))
+			places[word[i]] = vector<int> (1,i);
+		else
+			places[word[i]]->second.push_back(i);
+	}
+}
+
+int anagramNum(map<char, vector<int> > places, string word) {
+
+	string look;
+
+	
+
+}
+
+vector<int> anagrams(vector<string> words) {
+
+	int anagramNum;
+	map<char, vector<int> > places;
+	vecotr<int> totalAnagrams;
+
+	for (int i=0; i<words.size(); i++) {
+
+		getPlaces(places, words[i]);
+		anagramNum = getAnagrams(places, words[i]);
+		totalAnagrams.push_back(anagramNum);
+		places.clear();
+	}
+
+	return totalAnagrams;
+}
+
+int main() {
+	
+	return 0;
+}
