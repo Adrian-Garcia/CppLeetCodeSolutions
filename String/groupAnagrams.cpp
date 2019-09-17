15a171bb6d49b8c1659662c5e1619da31abf1837
Adrian Garcia <adriangarlop@hotmail.com>
2019-09-17 16:41:56 -0500

    Added groupAnagrams.cpp not accepted yet

---
+++ String/groupAnagrams.cpp
@@ -0,0 +1,104 @@
+/* 49. Group Anagrams			Medium
+
+	Given an array of strings, group anagrams together.
+
+	Example:
+
+		Input:
+			["eat", "tea", "tan", "ate", "nat", "bat"],
+
+		Output:
+			[
+			  ["ate","eat","tea"],
+			  ["nat","tan"],
+			  ["bat"]
+			]
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct Word {
+
+	string unorder;
+	string ordered;
+
+	Word(string w) : unorder(w), ordered(w) {}
+};
+
+bool wordsOrder(Word w1, Word w2) {
+
+	if (w1.ordered < w2.ordered)
+		return true;
+
+	return false;
+}
+
+vector<vector<string> > groupAnagrams(vector<string>& strs) {
+
+	vector<vector<string> > res;
+	vector<Word> words;
+	string curr;
+	int counter;
+
+	if (strs.size() == 0) 
+		return res;
+
+	if (strs.size() == 1 && strs[0] == "")
+		return res; 
+
+	for (int i=0; i<strs.size(); i++)
+		words.push_back(Word(strs[i]));
+
+	for (int i=0; i<words.size(); i++)
+		sort(words[i].ordered.begin(), words[i].ordered.end());
+
+	sort(words.begin(), words.end(), wordsOrder);
+
+	curr = "";
+	counter = -1;
+
+	for (int i=0; i<words.size(); i++) {
+
+		if (curr == words[i].ordered) 
+			res[counter].push_back(words[i].unorder);
+
+		else {
+
+			curr = words[i].ordered;
+			counter++;
+
+			res.push_back(vector<string>());
+			res[counter].push_back(words[i].unorder);
+		}
+	} 
+
+	return res;
+}
+
+int main() {
+
+	vector<string> strs;
+
+	strs.push_back("eat");
+	strs.push_back("tea");
+	strs.push_back("tan");
+	strs.push_back("ate");
+	strs.push_back("nat");
+	strs.push_back("bat");
+
+	vector<vector<string> > v = groupAnagrams(strs);
+
+	for (int i=0; i<v.size(); i++) {
+		for (int j=0; j<v[i].size(); j++) {
+			cout << v[i][j] << " "; 
+		} cout << endl;
+	}
+
+	return 0;
+}
